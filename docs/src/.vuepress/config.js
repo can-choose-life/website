b81350e386e534968a79332a24fcca83d2bc9646
peace <modepaes@gmail.com>
2023-10-22 21:30:20 +0300

    Set title of page

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: '',
+  title: 'Обирай Життя',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
